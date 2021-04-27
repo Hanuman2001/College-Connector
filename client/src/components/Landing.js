20152437ec1a6ca1be4f1d1331a7032ffa3a529e
unknown <adalatshanidev@gmail.com>
2021-04-27 17:15:46 +0530

    Ready for Deployment

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router";
+import { Link } from "react-router-dom";
 const Landing = ({ register: { isAuthenticated } }) => {
   if (isAuthenticated) {
     return <Redirect to="/dashboard" />;
   }
 
-  return <div className="landing">Hello</div>;
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Developer Connector</h1>
+          <p className="lead">
+            Create a developer profile/portfolio, share posts and get help from
+            other developers
+          </p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-light">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 const mapStatetoProps = (state) => ({
   register: state.register,
